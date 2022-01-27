699750f431331e641c9a36212f946be397cfcd38
pust1n <91640299+pust1n@users.noreply.github.com>
2022-01-26 19:46:12 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import random
+
+print('Welcome To the Password Generator')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*().,?0123456789'
+
+number = input('Amount of passwords to generate: ')
+number = int(number)
+
+length = input('Input your password length: ')
+length = int(length)
+
+print('/nhere are your passwords:') 
+
+for pwd in range(number):
+  
